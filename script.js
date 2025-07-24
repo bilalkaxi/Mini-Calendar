dfac41fa401947c03e66870a15fccb42ac872035
Qazi Muhammad Bilal Jamil <129346837+bilalkaxi@users.noreply.github.com>
2025-07-24 20:26:22 +0500

    Add files via upload
    Mini-Calendar

---
+++ script.js
@@ -0,0 +1,17 @@
+const monthEl = document.getElementById("month");
+const dayNameEl = document.getElementById("day-name");
+const dayNumEl = document.getElementById("day-num");
+const yearEl = document.getElementById("year");
+
+const date = new Date();
+const month = date.getMonth();
+
+monthEl.innerText = date.toLocaleString("en", {
+    month: "long",
+});
+
+dayNameEl.innerText = date.toLocaleString("en", {
+    weekday: "long",
+});
+ dayNumEl.innerText = date.getDate();
+ yearEl.innerText = date.getFullYear();
